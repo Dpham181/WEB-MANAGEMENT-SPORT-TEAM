bc8114f19c4a9b4ab13c1c0b713ec6fac5b6e6da
Thomas Ngo <tngo0508@gmail.com>
2018-04-18 18:35:29 -0700

    fix schedule game table

--- schedule_game.php
+++ schedule_game.php
@@ -18,25 +18,10 @@
           die("ERROR: Could not connect. " . mysqli_connect_error());
       }
 
-      $query = " SELECT
-        G.GAME_ID,G.START_DAY, G.END_DAY,
-        T1.TEAM_NAME, T1.WIN, T1.LOSS,
-        T2.TEAM_NAME, T2.WIN, T2.LOSS
-        FROM
-        GAMES as G,
-        TEAMS as T1,
-        TEAMS as T2,
-        PLAY  as P
-        WHERE
-        G.GAME_ID   = P.PGAME_ID AND
-        (P.PTEAM_ID = T1.TEAM_ID OR P.PTEAM_ID = T2.TEAM_ID) AND
-        T1.TEAM_ID != T2.TEAM_ID
-        GROUP BY
-        G.GAME_ID
-        ORDER BY
-        G.START_DAY,
-        T1.TEAM_NAME;
-        ";
+      $query = "SELECT G.GAME_ID, G.START_DAY, G.END_DAY, T.TEAM_NAME, T.WIN, T.LOSS
+      FROM PLAY P, GAMES G, TEAMS T
+      WHERE T.TEAM_ID = P.PTEAM_ID AND G.GAME_ID = P.PGAME_ID
+      ORDER BY GAME_ID;";
 
       if ($stmt = $link->prepare($query)) {
         $stmt->execute();
@@ -45,21 +30,16 @@
           $GID,
           $STD,
           $ETD,
-          $TN1,
-          $W1,
-          $L1,
-          $TN2,
-          $W2,
-          $L2
+          $TN,
+          $W,
+          $L
         );
       }
     ?>
 
 
     <?php
-      if ($stmt =  $link->prepare($query)) {
         echo "GAME:  ".$stmt->num_rows. "<br/>";
-      }
     ?>
 
 
@@ -73,96 +53,36 @@
           <th scope="col">TEAM NAME</th>
           <th scope="col">WIN</th>
           <th scope="col">LOSS</th>
-          <!-- <th scope="col">% WIN</th> -->
 
           <th scope="col">TEAM NAME</th>
           <th scope="col">WIN</th>
           <th scope="col">LOSS</th>
-          <!-- <th scope="col">% WIN</th> -->
         </tr>
       </thead>
-        <!-- <tr>
-          <th style="vertical-align:top; border:1px solid black; background: lightgreen;">GAME</th>
-          <th style="vertical-align:top; border:1px solid black; background: lightgreen;">START_DAYS</th>
-          <th style="vertical-align:top; border:1px solid black; background: lightgreen;">END_DAYS</th>
-
-          <th style="vertical-align:top; border:1px solid black; background: lightgreen;">TEAM A NAME</th>
-          <th style="vertical-align:top; border:1px solid black; background: lightgreen;">WIN  </th>
-          <th style="vertical-align:top; border:1px solid black; background: lightgreen;">LOSS</th>
-          <th style="vertical-align:top; border:1px solid black; background: lightgreen;">% WIN</th>
-
-          <th style="vertical-align:top; border:1px solid black; background: lightgreen;">TEAM B NAME</th>
-          <th style="vertical-align:top; border:1px solid black; background: lightgreen;">WIN  </th>
-          <th style="vertical-align:top; border:1px solid black; background: lightgreen;">LOSS</th>
-          <th style="vertical-align:top; border:1px solid black; background: lightgreen;">% WIN</th>
-        </tr> -->
-
-
       <?php
-        // if ($stmt =  $link->prepare($query)) {
-        $row = 0;
 
-        echo "<tr>\n";
-
-
-          echo "<th scope=\"row\">".$GID."</th>\n";
-          echo "<td>".$STD."</td>\n";
-          echo "<td>".$ETD."</td>\n";
-
-          echo "<td>".$TN1."</td>\n";
-          echo "<td>".$W1."</td>\n";
-          echo "<td>".$L1."</td>\n";
-
-          echo "<td>".$TN2."</td>\n";
-          echo "<td>".$W2."</td>\n";
-          echo "<td>".$L2."</td>\n";
-        echo "</tr>\n";
         while($stmt->fetch()){
 
           echo "<tr>\n";
-
-
           echo "<th scope=\"row\">".$GID."</th>\n";
           echo "<td>".$STD."</td>\n";
           echo "<td>".$ETD."</td>\n";
 
-          echo "<td>".$TN1."</td>\n";
-          echo "<td>".$W1."</td>\n";
-          echo "<td>".$L1."</td>\n";
+          echo "<td>".$TN."</td>\n";
+          echo "<td>".$W."</td>\n";
+          echo "<td>".$L."</td>\n";
 
-          echo "<td>".$TN2."</td>\n";
-          echo "<td>".$W2."</td>\n";
-          echo "<td>".$L2."</td>\n";
-            // echo "<td>".$PERSENT."</td>\n";
-        //   $stmt1->fetch();
-        // echo "<td  style='vertical-align:top; border:1px solid black;'>". $TN ."</td>\n";
-        // echo "<td style='vertical-align:top; border:1px solid black;'> ". $W ."</td>\n";
-        //   echo "<td style='vertical-align:top; border:1px solid black;'>". $L ."</td>\n";
-        //     echo "<td style='vertical-align:top; border:1px solid black;'>". $PERSENT . '%' ."</td>\n";
-        //   $stmt1->fetch();
-        //   echo "<td  style='vertical-align:top; border:1px solid black;'>". $TN ."</td>\n";
-        //   echo "<td style='vertical-align:top; border:1px solid black;'> ". $W ."</td>\n";
-        //   echo "<td style='vertical-align:top; border:1px solid black;'>". $L ."</td>\n";
-        //   echo "<td style='vertical-align:top; border:1px solid black;'>". $PERSENT . '%' ."</td>\n";
-
-
-
+          $stmt->fetch();
+          echo "<td>".$TN."</td>\n";
+          echo "<td>".$W."</td>\n";
+          echo "<td>".$L."</td>\n";
           echo "</tr>";
 
-}
-
-
+        }
 
         $stmt->free_result();
 
-
-
-
-
-
-
         $link->close();
-      // }
 
       ?>
 
