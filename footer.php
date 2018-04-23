a2c9f582d8b7dadd329b7f5f1965053cfad02f85
Thomas Ngo <tngo0508@gmail.com>
2018-04-23 16:05:06 -0700

    modify footer

--- footer.php
+++ footer.php
@@ -2,4 +2,12 @@
   <footer>
     <link rel="stylesheet" href="css/footer.css">
       <p>&copy; CPSC431 SPORTS TEAM WEBSITE DESIGNING.<br />
+        <h4>Follow us on</h4>
+        <div class="fa-3x">
+          <i class="fab fa-facebook-square"></i>
+          <i class="fab fa-twitter-square"></i>
+					<i class="fab fa-google-plus-square"></i>
+        </div>
+
+
   </footer>
--- forget_passform.php
+++ forget_passform.php
@@ -58,6 +58,8 @@
     <link rel="stylesheet" href="css/forgot_pass.css">
     <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Tangerine:bold,bolditalic|Inconsolata:italic|Droid+Sans|Oxygen|Passion+One|Alfa+Slab+One|Monoton|Ubuntu">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Rancho&effect=shadow-multiple|3d-float|fire-animation">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
+
 
   </head>
 
