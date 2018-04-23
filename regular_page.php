0d7e657d8db703bd9b1a4a44a63396ed665cd082
Thomas Ngo <tngo0508@gmail.com>
2018-04-23 16:21:08 -0700

    add carousel for regular page

--- regular_page.php
+++ regular_page.php
@@ -70,9 +70,35 @@
 </div>
 
 
-
+<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <div class="container">
+        <?php require ('schedule_game.php'); ?>
+      </div>
 
     </div>
+    <div class="carousel-item">
+      <div class="container">
+      <?php require 'User_viewupcominggame.php'; ?>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <div class="container">
+    <?php require 'User_viewStats.php'; ?>
+      </div>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
 
 
 
