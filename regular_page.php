7db19b4588abb46ce298b8518ecd63f24e7be165
danhpham <danhpham312@gmail.com>
2018-04-23 14:38:34 -0700

    Add files via upload

--- regular_page.php
+++ regular_page.php
@@ -1,13 +1,6 @@
 <?php
      require "header.php";
      session_start();
-     if ($_SESSION['type'] !== 'U') {
-       $_SESSION = array();
-       session_destroy();
-       header("location: welcome.php");
-       exit;
-     }
-
      if(!isset($_SESSION['email']) || empty($_SESSION['email'])){
        header("location: welcome.php");
        exit;
@@ -25,66 +18,67 @@
 </head>
 
 <body>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-
-    <a class="navbar-brand" href="#">MENU</a>
-
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-  <span class="navbar-toggler-icon"></span>
-</button>
-    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul class="navbar-nav">
-        <li class="nav-item active">
-
-          <a class="nav-link" href="regular_page.php">Home </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="view_games.php">Schedule</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="view_standings.php">Standings</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="user_view_stats.php">Stats</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="view_teams.php">Teams</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="rules.php">Rules</a>
-
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="contact.php">Contact</a>
-        </li>
-
-        <li id="info"class="nav-item active">
-        <a class="nav-link"    <b><?php
-          echo "Hi, " .$_SESSION['email'];
-           ?></b>.
-
-        </a>
-        </li>
-
-        <li id="Profile"class="nav-item active">
-          <a class="nav-link" href="update_profile.php">PROFILE</a>
-
-        </li>
-        <li id="logout"class="nav-item active">
-          <a class="nav-link" href="logout.php">SIGN OUT</a>
-        </li>
-      </ul>
-    </div>
-  </nav>
-
-  <section id="body">
-      <div class="container">
-
-        <h1>Welcome to your user page.  Use the navigation menu above.</h1>
-
-
+  <ul class="nav nav-tabs" id="myTab" role="tablist">
+    <li class="nav-item">
+      <a class="navbar-brand" href="regular_page.php">TEAM MANAGEMENT</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" id="schedule-tab" data-toggle="tab" href="#schedule" role="tab" aria-controls="schedule" aria-selected="false">Schedule Game</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" id="upcome-tab" data-toggle="tab" href="#upcome" role="tab" aria-controls="upcome" aria-selected="false">Up Coming Games</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" id="view-tab" data-toggle="tab" href="#view" role="tab" aria-controls="view" aria-selected="false">Player's Stats</a>
+    </li>
+    <li class="nav-item dropdown">
+      <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+        <?php echo $_SESSION['email']; ?>
+      </a>
+      <div class="dropdown-menu">
+        <a class="dropdown-item" href="check_profileEmpty.php">Profile</a>
+        <a class="dropdown-item" href="update_profile.php">Edit Profile</a>
+        <a class="dropdown-item" href="regular_page.php">home page</a>
+        <div class="dropdown-divider"></div>
+        <a class="dropdown-item" href="logout.php">Log out</a>
       </div>
-  </section>
+    </li>
+  </ul>
+
+
+  <div class="tab-content" id="myTabContent">
+    <div class="tab-pane fade" id="schedule" role="tabpanel" aria-labelledby="schedule-tab">
+      <h1>SCHEDULE ALL GAMES</h1>
+      <div class="container">
+        <?php require ('schedule_game.php'); ?>
+      </div>
+    </div>
+    <div class="tab-pane fade" id="upcome" role="tabpanel" aria-labelledby="upcoming-tab">
+    <h1 >UPCOMING GAMES</h1>
+    <div class="container">
+      <?php require 'User_viewupcominggame.php'; ?>
+    </div>
+  </div>
+
+  <div class="tab-pane fade" id="view" role="tabpanel" aria-labelledby="view-tab">
+  <h1 >PLAYER'S STATS</h1>
+  <div class="container">
+    <?php require 'User_viewStats.php'; ?>
+  </div>
+</div>
+
+
+
+
+    </div>
+
+
+
+
+
+
+
+
 
 
   <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
