e0a327b1c7c8512555cb226de4e8b98f039298a6
Thomas Ngo <tngo0508@gmail.com>
2018-04-23 14:52:02 -0700

    update

--- User_viewupcominggame.php
+++ User_viewupcominggame.php
@@ -1,10 +1,4 @@
 <?php
-
-
-
-
-
-
 require_once('config.php');
 // Connect to database
 /* Attempt to connect to MySQL database */
@@ -13,8 +7,7 @@ $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
-$query ="SELECT GAME_ID, START_DAY, END_DAY FROM GAMES WHERE NOT EXISTS(SELECT PGAME_ID FROM PLAY WHERE GAME_ID = PGAME_ID)
-";
+$query ="SELECT GAME_ID, START_DAY, END_DAY FROM GAMES WHERE GAME_ID IN (SELECT PGAME_ID FROM PLAY WHERE SCORE IS NULL)";
 $stmt = $link->prepare($query);
 $stmt->execute();
 $stmt->store_result();
@@ -23,6 +16,17 @@ $ID,
 $TID,
 $S
 );
+?>
+<table class="table table-bordered table-hover">
+    <thead class="thead-dark">
+      <tr class="info">
+        <th scope="col">GAME ID</th>
+        <th scope="col">START DAY</th>
+        <th scope="col">END DAY</th>
+      </tr>
+  </thead>
+
+<?php
 while($stmt->fetch()){
 echo "<tr>\n";
 echo "<td>".$ID."</td>\n";
@@ -41,23 +45,5 @@ $stmt->free_result();
 
 $link->close();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 ?>
+</table>
