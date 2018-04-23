04a6ed0fab872f6e3acf5480b2dacef75d1bf9bd
Thomas Ngo <tngo0508@gmail.com>
2018-04-23 16:38:47 -0700

    add href link for icons in footer

--- footer.php
+++ footer.php
@@ -4,9 +4,10 @@
       <p>&copy; CPSC431 SPORTS TEAM WEBSITE DESIGNING.<br />
         <h4>Follow us on</h4>
         <div class="fa-3x">
-          <i class="fab fa-facebook-square"></i>
-          <i class="fab fa-twitter-square"></i>
-					<i class="fab fa-google-plus-square"></i>
+
+          <a href="https://www.facebook.com/"><i class="fab fa-facebook-square"></i></a>
+          <a href="https://twitter.com/"><i class="fab fa-twitter-square"></i></a>
+          <a href="https://plus.google.com/discover"><i class="fab fa-google-plus-square"></i></a>
         </div>
 
 
